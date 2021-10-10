748ce9c89a9325ad30e9f45f137a54685db7401e
sami gharbi <sami.gharbi@gmail.com>
2021-10-10 12:35:17 +0200

    first commit

---
+++ src/main/java/com/sami/centermanager/CenterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sami.centermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sami/centermanager/CenterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sami.centermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
